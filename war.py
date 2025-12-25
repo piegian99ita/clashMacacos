1443c89ebe633a93420c6f57c79b97518b4312f7
piegian99ita <pietro.giannini@studenti.unitn.it>
2025-12-26 00:20:59 +0100

    Update war.py

--- war.py
+++ war.py
@@ -7,11 +7,9 @@ import openpyxl
 from openpyxl.utils import get_column_letter
 
 # --- CONFIGURAZIONE ---
-# Inserisci qui i dati o caricali da file
-COC_EMAIL = "ilredeitrattori@gmail.com"
-COC_PASSWORD = "3499Pg00"
-# Il Tag del clan può essere letto dal file o impostato qui
-CLAN_TAG = "2JPPL0922"
+COC_EMAIL = os.getenv("COC_EMAIL")
+COC_PASSWORD = os.getenv("COC_PASSWORD")
+CLAN_TAG = os.getenv("CLAN_TAG")
  
 
 def copia_stile(cella_origine, cella_destinazione):
